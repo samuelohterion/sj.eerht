748d0d3c27e7f3c27b02261d3e03067f2760cc65
Thomas Peschel <tpeschel@life.uni-leipzig.de>
2016-12-30 15:36:41 +0100

    open new branch due to change fabrique

--- realPhysics/fabrique/fabrique.js
+++ realPhysics/fabrique/fabrique.js
@@ -1,5 +1,10 @@
 My = {
 
+	sigmoid : function ( pX ) {
+
+		return 2. / ( 1. + Math.exp ( -2. * pX ) ) - 1.;
+	},
+
 	Spring : function ( pId0, pId1, pVertices ) {
 
 		this.i0 = pId0;
@@ -18,19 +23,23 @@ My = {
 		this.springs        = [ ];
 		this.vel			= [ ];
 		this.acc			= [ ];
-		this.lnks			= [ ];
+		this.lnk			= [ ];
+		this.vrt			= [ ];
+		this.vtmp			= [ ];
 
 		var
 		area = this.width * this.width,
-		offs = this.width >> 1;
+		offs = .5 * ( this.width - 1 );
 
 		for ( var z = 0; z < this.width; ++z ) {
 
 			for ( var x = 0; x < this.width; ++x ) {
 
 				this.mesh.geometry.vertices.push ( new THREE.Vector3 ( x - offs, 0. * ( 2 * Math.random ( ) - 1 ), z - offs ) );
-				this.acc.push( new THREE.Vector3 ( ) );
-				this.vel.push( new THREE.Vector3 ( ) );
+				this.vrt.push ( new THREE.Vector3 ( ) );
+				this.acc.push ( new THREE.Vector3 ( ) );
+				this.vel.push ( new THREE.Vector3 ( ) );
+				this.vtmp.push ( new THREE.Vector3 ( ) );
 				this.lnk.push ( 0 );
 			}
 		}
@@ -43,8 +52,8 @@ My = {
 				from = x * this.width + z,
 				to   = x * this.width + z + 1;
 
-				++this.lnks[ from ];
-				++this.lnks[ to ];
+				++this.lnk[ from ];
+				++this.lnk[ to ];
 
 				this.springs.push ( new My.Spring ( from, to, this.mesh.geometry.vertices ) );
 			}
@@ -58,8 +67,8 @@ My = {
 				from = x * this.width + z,
 				to   = ( x + 1 ) * this.width + z;
 
-				++this.lnks[ from ];
-				++this.lnks[ to ];
+				++this.lnk[ from ];
+				++this.lnk[ to ];
 
 				this.springs.push ( new My.Spring ( from, to, this.mesh.geometry.vertices ) );
 			}
@@ -73,16 +82,16 @@ My = {
 				from = x * this.width + z,
 				to   = ( x + 1 ) * this.width + z + 1;
 
-				++this.lnks[ from ];
-				++this.lnks[ to ];
+				++this.lnk[ from ];
+				++this.lnk[ to ];
 
 				this.springs.push ( new My.Spring ( from, to, this.mesh.geometry.vertices ) );
 
 				from = x * this.width + z + 1;
 				to   = ( x + 1 ) * this.width + z;
 
-				++this.lnks[ from ];
-				++this.lnks[ to ];
+				++this.lnk[ from ];
+				++this.lnk[ to ];
 
 				this.springs.push ( new My.Spring ( from, to, this.mesh.geometry.vertices ) );
 			}
@@ -102,7 +111,7 @@ My = {
 				this.mesh.geometry.faces.push ( new THREE.Face3( i1, i3, i2 ) );
 			}
 		}
-*
+/*
 		this.mesh.geometry.verticesNeedUpdate = true;
 		this.mesh.geometry.normalsNeedUpdate  = true;
 		this.mesh.geometry.colorsNeedUpdate   = true;
@@ -116,53 +125,52 @@ My = {
 
 My.Spring.prototype = {
 
-	constructor : My.Spring
+	constructor : My.Spring,
+
+	sigmoIt     : function ( pVrtDst, pVrtSrc ) {
+
+		var
+		v0 = pVrtSrc[ this.i0 ],
+		v1 = pVrtSrc[ this.i1 ],
+		dV = v1.clone ( ).sub ( v0 ),
+		d  = dV.length ( ),
+		l  = ( d - this.l ) / this.l,
+		l2 = ( 1. + My.sigmoid ( l ) ) * this.l;
+
+		dV.multiplyScalar( .5 * l2 / d );
+
+		var
+		dC = ( v0.clone ( ).add ( v1 ) ).multiplyScalar ( .5 ),
+		dV0 = dC.clone ( ).sub ( dV ),
+		dV1 = dC.clone ( ).add ( dV );
+
+		pVrtDst[ this.i0 ].add ( dV0 );
+		pVrtDst[ this.i1 ].add ( dV1 );
+	}
 }
 
 My.Fabrique.prototype = {
 
 	constructor   : My.Fabrique,
-	sigmoid       : function ( pV, pLen = 1., pDX = .2, pDY = 1. ) {
+	sigmoIt       : function ( ) {
 
-		var
-		s = new THREE.Vector3 ( 0, 0, 0 );
+		for ( var i = 0; i < this.springs.length; ++i ) {
 
-		for ( var i = 0; i < this.lnk[ pId ].length; ++i ) {
-
-			s.add( this.vertices[ pId ].this.lnk[ pId ][ i ] );
+			this.springs[ i ].sigmoIt ( this.mesh.geometry.vertices, this.vrt );
 		}
 
-		s.divideScalar( this.lnk[ pId ].length );
+		for ( var i = 0; i < this.mesh.geometry.vertices.length; ++i ) {
 
-		var
-		v = this.mesh.geometry.vertices[ pId ]. clone ( ),
-		l = s.sub ( v ).length ( ),
-		l2 = pDY * ( 2.  / ( 1. + Math.exp( -l / pDX )  ) - 1. );
-
-		return v.multiplyScalar( l2 / l )
-	},
-
-	sigmoIt       : function ( pDX = 1., pDY = 1. ) {
-
-		var
-		s = new THREE.Vector3 ( 0, 0, 0 );
-
-		for ( var i = 0; i < this.lnk[ pId ].length; ++i ) {
-
-			s.add( this.vertices[ pId ].this.lnk[ pId ][ i ] );
+			this.mesh.geometry.vertices[ i ].divideScalar( this.lnk[ i ] );
 		}
-
-		s.divideScalar( this.lnk[ pId ].length );
-
-		var
-		v = this.mesh.geometry.vertices[ pId ]. clone ( ),
-		l = s.sub ( v ).length ( ),
-		l2 = pDY * ( 2.  / ( 1. + Math.exp( -l / pDX )  ) - 1. );
-
-		return v.multiplyScalar( l2 / l )
 	},
 
 	live : function ( pDt ) {
+
+		for ( var i = 0; i < this.mesh.geometry.vertices.length; ++i ) {
+
+			this.vtmp[ i ].copy ( this.mesh.geometry.vertices[ i ] );
+		}
 
 		for ( var i = 0; i < this.springs.length; ++i ) {
 
@@ -181,17 +189,30 @@ My.Fabrique.prototype = {
 			a1.sub ( d.clone( ) );
 		}
 
+		for ( var i = 0; i < this.width; ++i ) {
+
+			this.vrt[ i ].copy( this.mesh.geometry.vertices[ i ] );
+		}
+
 		for ( var i = this.width; i < this.mesh.geometry.vertices.length; ++i ) {
 
-			var
-			mass = this.mesh.geometry.vertices[ i ];
-
 			this.acc[ i ].y -= 9.81;
-			mass.add( this.vel[ i ].clone ( ).multiplyScalar( pDt ) );
-			this.vel[ i ].add( this.acc[ i ].clone ( ).multiplyScalar( pDt ).sub( this.vel[ i ].clone ( ).multiplyScalar( .1 ) ) );
-//			this.vel[ i ].add( this.acc[ i ].clone ( ).multiplyScalar( pDt ) );
+			this.vrt[ i ].copy( this.vel[ i ].clone ( ).multiplyScalar( pDt ).add ( this.mesh.geometry.vertices[ i ] ) );
+//			this.vel[ i ].add( this.acc[ i ].clone ( ).multiplyScalar( pDt ).sub( this.vel[ i ].clone ( ).multiplyScalar( .1 ) ) );
+			this.vel[ i ].add( this.acc[ i ].clone ( ).multiplyScalar( pDt ) );
 			this.acc[ i ].set ( 0, 0, 0 );
 		}
-		this.sigmoid( .1, 1. );
+
+		for ( var i = 0; i < this.mesh.geometry.vertices.length; ++i ) {
+
+			this.mesh.geometry.vertices[ i ].set ( 0, 0, 0 );
+		}
+
+		this.sigmoIt( );
+
+		for ( var i = 0; i < this.vel.length; ++i ) {
+
+			this.vel[ i ].copy( ( this.mesh.geometry.vertices[ i ].clone ( ).sub ( this.vtmp[ i ] ) ).divideScalar ( pDt ) );
+		}
 	}
 }
